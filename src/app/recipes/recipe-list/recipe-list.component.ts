b31159efb7851dad5e77eb21da0e77ae2b43f31c
robin-john-menaa <johnrobin80@gmail.com>
2021-10-02 22:22:49 +0400

    Coding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
       'This is just for testing',
       'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
     ),
-        new Recipe(
+    new Recipe(
       'A test recipe2',
       'This is just for testing2',
       'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
     ),
-            new Recipe(
+    new Recipe(
       'A test recipe3',
       'This is just for testing3',
       'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
     ),
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onRecipeSelected(recipe: Recipe) {}
 }

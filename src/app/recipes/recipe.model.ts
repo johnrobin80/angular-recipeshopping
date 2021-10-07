58c11108d859973cb989c8dad054914a76302391
robin-john-menaa <robinprojectgcp@gmail.com>
2021-10-07 18:42:01 +0400

    Updates

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,21 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
+  //public name: string;
+  //public description: string;
+  //public imagePath: string;
 
-  constructor(name: string, desc: string, imagePath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
+  //constructor(name: string, desc: string, imagePath: string) {
+  //  this.name = name;
+  //  this.description = desc;
+  //  this.imagePath = imagePath;
+  //}
+
+  constructor(
+    public name: string,
+    public description: string,
+    public imagePath: string
+  ) {
+    this.name;
+    this.description;
+    this.imagePath;
   }
 }

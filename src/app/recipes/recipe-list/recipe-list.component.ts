cc036ccf3508d044a51e2a7b31d02dd72c60a877
robin-john-menaa <robinprojectgcp@gmail.com>
2021-10-18 18:54:02 +0400

    Adding service class to recipie

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,29 +9,32 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe',
-      'This is just for testing',
-      'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
-    ),
-    new Recipe(
-      'A test recipe2',
-      'This is just for testing2',
-      'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
-    ),
-    new Recipe(
-      'A test recipe3',
-      'This is just for testing3',
-      'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
-    ),
-  ];
-  constructor() {}
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] | any;
+  // recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A test recipe',
+  //     'This is just for testing',
+  //     'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
+  //   ),
+  //   new Recipe(
+  //     'A test recipe2',
+  //     'This is just for testing2',
+  //     'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
+  //   ),
+  //   new Recipe(
+  //     'A test recipe3',
+  //     'This is just for testing3',
+  //     'https://cto-food.com/swfiles/files/shutterstock_203111092.jpg?nc=1620636472'
+  //   ),
+  // ];
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
+
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Smokey Salmon',
+      'Smokey salmon with steamed vegetables.',
+      'https://lh3.google.com/u/0/d/1v47_krDTciIiDbnI1iPmf4chDqlvTDkF=w1919-h937-iv1'
+    ),
+    new Recipe(
+      'Grilled Chicken',
+      'Tender cheicken breast grilled with flavoured leafs and ginger-garlic paste.',
+      'https://lh3.google.com/u/0/d/1c7HyGodwDLju5-1_cCo6C9CgVkgRbOop=w1920-h937-iv1'
+    ),
+    new Recipe(
+      'Chicken Cheese Roast',
+      'Chicken roasted with cheese dressing.',
+      'https://lh3.google.com/u/0/d/14o8UOUPjPMJR9hJQSpNctWUmTXyfw0dM=w1285-h937-iv1'
+    ),
+  ];
   constructor() {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }

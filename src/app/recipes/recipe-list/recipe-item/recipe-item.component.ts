4faeaad2b5846c764776a52daccedfb71061c5f7
robin-john-menaa <robinprojectgcp@gmail.com>
2021-11-24 19:30:07 +0400

    Routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,6 +10,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe | any;
+  @Input() index: number | any;
 
   //@Output() recipeSelected = new EventEmitter<void>();
 
